f5917300627fc6748c472992ef596a016d334026
lixiaocong <960418051@qq.com>
2017-05-21 13:07:22 +0800

    add ng-lighting

--- admin/src/app/pages/article/article.component.ts
+++ admin/src/app/pages/article/article.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import {INglDatatableSort, INglDatatableRowClick} from 'ng-lightning/ng-lightning';
 
+const DATA = [
+  { rank: 1, name: 'Kareem', surname: 'Abdul-Jabbar', points: 38387 },
+  { rank: 2, name: 'Karl', surname: 'Malone', points: 36928 },
+  { rank: 3, name: 'Kobe', surname: 'Bryant', points: 33643 },
+  { rank: 4, name: 'Michael', surname: 'Jordan', points: 32292 },
+  { rank: 5, name: 'Wilt', surname: 'Chamberlain', points: 31419 },
+];
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
   styleUrls: ['./article.component.css']
 })
-export class ArticleComponent implements OnInit {
+export class ArticleComponent {
 
-  constructor() { }
+  data = DATA;
 
-  ngOnInit() {
+  // Initial sort
+  sort: INglDatatableSort = { key: 'rank', order: 'asc' };
+
+  // Show loading overlay
+  loading = false;
+
+  // Toggle name column
+  hideName = false;
+
+  // Custom sort function
+  onSort($event: INglDatatableSort) {
+    const { key, order } = $event;
+    this.data.sort((a: any, b: any) => {
+      return (key === 'rank' ? b[key] - a[key] : b[key].localeCompare(a[key])) * (order === 'desc' ? 1 : -1);
+    });
   }
 
+  toggleData() {
+    this.data = this.data ? null : DATA;
+  }
+
+  onRowClick($event: INglDatatableRowClick) {
+    console.log('clicked row', $event.data);
+  }
 }
