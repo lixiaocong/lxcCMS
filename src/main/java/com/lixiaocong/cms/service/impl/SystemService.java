8f3f30f6410101135bf9935a7179b338e74b4994
lixiaocong <960418051@qq.com>
2018-03-21 21:22:24 +0800

    fix normal users have access to admin panel bug
    1. add global method security annotation
    2. remove some unnecessary dependencies of Angular
    3. add SystemService

--- src/main/java/com/lixiaocong/cms/service/impl/SystemService.java
+++ src/main/java/com/lixiaocong/cms/service/impl/SystemService.java
@@ -30,34 +30,34 @@
   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package com.lixiaocong.cms.config;
+package com.lixiaocong.cms.service.impl;
 
-import com.lixiaocong.cms.security.DaoBasedUserDetailsService;
+import com.lixiaocong.cms.service.IConfigService;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
 
-@Configuration
-@EnableGlobalMethodSecurity(jsr250Enabled = true) // this is the key to enable RolesAllowed annotation
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    private final DaoBasedUserDetailsService detailsService;
+import java.io.File;
+
+@Service
+public class SystemService {
+    private static final Log log = LogFactory.getLog(SystemService.class);
+
+    private final IConfigService configService;
 
     @Autowired
-    public WebSecurityConfig(DaoBasedUserDetailsService detailsService) {
-        this.detailsService = detailsService;
+    public SystemService(IConfigService configService) {
+        this.configService = configService;
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().permitAll().and().formLogin().loginPage("/signin").defaultSuccessUrl("/blog").and().logout().logoutUrl("/logout").logoutSuccessUrl("/blog").and().rememberMe().rememberMeParameter("remember-me").and().csrf().disable();
+    public String getFreeSpace(){
+        File file = new File(configService.getStorageDir());
+        return FileUtils.byteCountToDisplaySize(file.getUsableSpace());
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(detailsService).passwordEncoder(new BCryptPasswordEncoder());
+    public String getFileSize(String path){
+        return FileUtils.byteCountToDisplaySize(FileUtils.sizeOf(new File(path)));
     }
 }
